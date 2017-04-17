48b7fb4a2797351e5baaf2ddba00c2453d97f33b
Raviprasad <raviprasad@tavant.com>
2017-04-17 14:55:30 +0530

    initial commit

---
+++ src/main/java/com/sam/ravi/dto/JsonFields.java
@@ -0,0 +1,13 @@
+package com.sam.ravi.dto;
+
+public class JsonFields {
+	private Fields[] fields;
+
+	public Fields[] getfields() {
+		return fields;
+	}
+	public void setType(Fields[] fields) {
+		this.fields = fields;
+	}
+
+}
---
+++ src/main/java/com/sam/ravi/service/AppService.java
@@ -0,0 +1,7 @@
+package com.sam.ravi.service;
+
+import com.sam.ravi.dto.JsonFields;
+
+public interface AppService {
+	JsonFields getFromjsonFile();
+}
